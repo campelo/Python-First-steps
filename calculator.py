d8e095d7618599f0d3b733bb2d55a3d8fd1704ba
Flavio Campelo <flaviocampelo@hotmail.com>
2022-01-12 08:35:25 -0500

    Calculator 4

--- calculator.py
+++ calculator.py
@@ -1,4 +1,4 @@
 firstNumber = int(input("Enter the first number: ")) # it will wait user input for the first number
 secondNumber = int(input("Enter the second number: "))  # it will wait user input for the second number
 total = firstNumber + secondNumber # it will summarize
-print("The result is: " + total)  # it will show the result
+print("The result is: " + str(total))  # it will show the result
